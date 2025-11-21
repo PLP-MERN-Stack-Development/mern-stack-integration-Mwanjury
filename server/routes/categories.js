fbd98c66e21299d3b610700c3ba9b9da633f256b
Mary Wambui <nyanjuiwambui@gmail.com>
2025-11-21 09:51:40 +0300

    Convert categories.js to ES6 module syntax

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
+import categoryController from '../controllers/categoryController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
-const categoryController = require('../controllers/categoryController');
-const { protect } = require('../middleware/authMiddleware');
 
 router.get('/', categoryController.getCategories);
 router.post('/', protect, categoryController.createCategory);
 
-module.exports = router;
+export default router;
+
