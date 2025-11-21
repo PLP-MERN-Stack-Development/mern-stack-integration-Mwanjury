ee836ae4963743ae90d87f961505e7c4ad5891b3
Mary Wambui <nyanjuiwambui@gmail.com>
2025-11-21 09:50:52 +0300

    Refactor auth.js to use ES6 module syntax

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const { body } = require('express-validator');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
+import express from 'express';
+import { body } from 'express-validator';
+import authController from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
-router.post('/register',
+const router = express.Router();
+
+router.post(
+  '/register',
   body('name').notEmpty().withMessage('Name required'),
   body('email').isEmail().withMessage('Valid email required'),
   body('password').isLength({ min: 6 }).withMessage('Password at least 6 chars'),
@@ -15,4 +17,4 @@ router.post('/login', authController.login);
 
 router.get('/me', protect, authController.me);
 
-module.exports = router;
+export default router;
