97c15057b6fc2ad90829126d52f0988564b0376f
Mary Wambui <nyanjuiwambui@gmail.com>
2025-11-21 09:53:52 +0300

    Refactor errorHandler to use ES6 export syntax

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,8 +1,10 @@
-exports.errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, next) => {
   console.error(err);
   const statusCode = err.statusCode || 500;
+
   res.status(statusCode).json({
     message: err.message || 'Server error',
     stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
   });
 };
+
